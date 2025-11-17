ce08aa84493ff34da15ac3bc31b3f7499e650064
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2025-11-17 17:55:47 +0200

    refactor(parser_app): allow blank values for Smartphone name field

--- braincomua_project/parser_app/models.py
+++ braincomua_project/parser_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Smartphone(models.Model):
-    name = models.CharField(max_length=255, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.name
