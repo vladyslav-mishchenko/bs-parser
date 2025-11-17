f4baa355a2a80330c131cf6a43f3a8c5d7868c65
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2025-11-17 17:13:36 +0200

    refactor(parser_app): allow null values for Smartphone name field

--- braincomua_project/parser_app/models.py
+++ braincomua_project/parser_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Smartphone(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return self.name
