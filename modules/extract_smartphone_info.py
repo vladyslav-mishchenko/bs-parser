946ae472b9194b18887fea932c69e22c1f317ffe
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2025-11-18 13:18:36 +0200

    chore(test): use local HTML file for BeautifulSoup testing

--- modules/extract_smartphone_info.py
+++ modules/extract_smartphone_info.py
@@ -4,11 +4,16 @@ from parser_app.models import Smartphone
 from utils.fetch_page_html import fetch_page_html
 from utils.parse_product_info import parse_product_info
 
-
 url = "https://brain.com.ua/ukr/Mobilniy_telefon_Apple_iPhone_16_Pro_Max_256GB_Black_Titanium-p1145443.html"
 
-html = fetch_page_html(url)
-data = parse_product_info(html)
+# comment for test process
+# html = fetch_page_html(url)
+
+# Open HTML page locally to learn BeautifulSoup more effectively
+with open("./files/page.html", "r", encoding="utf-8") as f:
+    local_html = f.read()
+
+data = parse_product_info(local_html)
 
 # save to db without defaults and unique
 smartphone, created = Smartphone.objects.get_or_create(name=data["name"])
