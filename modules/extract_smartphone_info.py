06dab25f1debf45cb0f6d321512c90d6c254178b
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2025-11-18 02:34:24 +0200

    feat(scraper): extract product name with BeautifulSoup and save to database (prints for testing)

--- modules/extract_smartphone_info.py
+++ modules/extract_smartphone_info.py
@@ -3,9 +3,17 @@ from parser_app.models import Smartphone
 from utils.fetch_page_html import fetch_page_html
 from bs4 import BeautifulSoup
 
-url = ""
+url = "https://brain.com.ua/ukr/Mobilniy_telefon_Apple_iPhone_16_Pro_Max_256GB_Black_Titanium-p1145443.html"
 html = fetch_page_html(url)
 
 soup = BeautifulSoup(html, "html.parser")
 
-print(soup.title.text)
+try:
+    name = soup.select_one("h1.desktop-only-title").get_text(strip=True)
+except AttributeError:
+    name = None
+
+smartphone = Smartphone.objects.create(name=name)
+
+for item in Smartphone.objects.all():
+    print(item.name)
