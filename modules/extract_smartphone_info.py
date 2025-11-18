f12141931b73eaa128270b22eac5c874b3a36281
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2025-11-18 11:06:45 +0200

    feat(db): save parsed data using get_or_create without unique constraints or defaults

--- modules/extract_smartphone_info.py
+++ modules/extract_smartphone_info.py
@@ -6,10 +6,19 @@ from utils.parse_product_info import parse_product_info
 
 
 url = "https://brain.com.ua/ukr/Mobilniy_telefon_Apple_iPhone_16_Pro_Max_256GB_Black_Titanium-p1145443.html"
+
 html = fetch_page_html(url)
 data = parse_product_info(html)
 
-smartphone = Smartphone.objects.create(name=data["name"])
+# save to db without defaults and unique
+smartphone, created = Smartphone.objects.get_or_create(name=data["name"])
 
+# print created or exists
+if created:
+    print(f"Created: {smartphone}")
+else:
+    print(f"Exists: {smartphone}")
+
+# print data from db
 for item in Smartphone.objects.all():
     print(item.name)
