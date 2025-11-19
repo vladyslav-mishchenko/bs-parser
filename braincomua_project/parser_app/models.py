f231970f732c561a27c72cfa38fcb6bf4efb5f09
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2025-11-19 04:44:22 +0200

    feat(parser): extract smartphone info and save to db

--- braincomua_project/parser_app/models.py
+++ braincomua_project/parser_app/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 class Smartphone(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
+    product_code = models.CharField(max_length=32, blank=True, null=True)
+    price = models.CharField(max_length=16, blank=True, null=True)
+    discounted_price = models.CharField(max_length=16, blank=True, null=True)
+    reviews = models.CharField(max_length=16, blank=True, null=True)
+    image_paths = ArrayField(
+        base_field=models.CharField(max_length=255), default=list, blank=True, null=True
+    )
+    characteristics = models.JSONField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.name if self.name is not None else "(no name)"
