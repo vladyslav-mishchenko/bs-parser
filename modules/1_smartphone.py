93a9d9f5ae8cee7ea8f28c749467fcaa4ca35dda
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2025-11-19 04:52:00 +0200

    fix: add renamed file

---
+++ modules/1_smartphone.py
@@ -0,0 +1,50 @@
+from load_django import *
+from parser_app.models import Smartphone
+
+from utils.fetch_page_html import fetch_page_html
+from utils.parse_product_info import parse_product_info
+
+url = "https://brain.com.ua/ukr/Mobilniy_telefon_Apple_iPhone_16_Pro_Max_256GB_Black_Titanium-p1145443.html"
+
+# comment for test process
+html = fetch_page_html(url)
+
+# Open HTML page locally to learn BeautifulSoup more effectively
+with open("../files/page.html", "r", encoding="utf-8") as f:
+    local_html = f.read()
+data = parse_product_info(local_html)
+
+# data = parse_product_info(html)
+# print(data)
+
+# save to db without defaults and unique
+smartphone, created = Smartphone.objects.get_or_create(
+    name=data["name"],
+    product_code=data["product_code"],
+    price=data["price"],
+    discounted_price=data["discounted_price"],
+    reviews=data["reviews"],
+    image_paths=data["images"],
+    characteristics=data["characteristics"],
+)
+
+# print created or exists
+if created:
+    print(f"Created: {smartphone}")
+else:
+    print(f"Exists: {smartphone}")
+
+# print data from db
+print("")
+print("----------------------------smartphones")
+for i in Smartphone.objects.all():
+    print(
+        f"Smartphone: {i.name} | "
+        f"Product Code: {i.product_code} | "
+        f"Price: {i.price} | "
+        f"Discounted Price: {i.discounted_price} | "
+        f"Reviews: {i.reviews} | "
+        f"Images: {i.image_paths} | "
+        f"Characteristics: {i.characteristics} "
+    )
+print("------------------------------------end")
--- modules/utils/parse_product_info.py
+++ modules/utils/parse_product_info.py
@@ -6,11 +6,44 @@ def extract_text(soup, selector):
     return el.get_text(strip=True) if el else None
 
 
+def extract_image_urls(soup, selector):
+    images = soup.select(selector)
+    if not images:
+        return None
+    urls = [img.get("src") for img in images if img.get("src")]
+    return urls or None
+
+
+def extract_characteristics(soup, selector):
+    characteristics = {}
+
+    items = soup.select(selector)
+    if not items:
+        return None
+
+    for item in items:
+        category = item.select_one("h3")
+        if not category:
+            continue
+
+        category_name = category.get_text(strip=True)
+        characteristics[category_name] = {}
+
+    return characteristics
+
+
 def parse_product_info(html):
     data = {}
     soup = BeautifulSoup(html, "html.parser")
 
     data["name"] = extract_text(soup, "h1.desktop-only-title")
-    data["product_key"] = extract_text(soup, ".br-pr-code-val")
+    data["product_code"] = extract_text(soup, ".br-pr-code-val")
+    data["price"] = extract_text(soup, ".main-price-block .br-pr-np")
+    data["discounted_price"] = extract_text(soup, ".main-price-block .br-pr-op")
+    data["reviews"] = extract_text(soup, ".main-comments-block .reviews-count")
+    data["images"] = extract_image_urls(soup, ".br-pr-slider .br-prs-s .br-main-img")
+    data["characteristics"] = extract_characteristics(
+        soup, ".br-pr-chr .br-pr-chr-item"
+    )
 
     return data
